21edd9c4889d21e90da9d7450c86f237afc9034d
Jean Carlo Garcia Soto <JeanGarciaSoto@outlook.com>
2025-05-07 21:57:42 -0600

    NPC Sam

--- Proyecto/Proyecto.cpp
+++ Proyecto/Proyecto.cpp
@@ -2157,11 +2157,42 @@ int main()
 
 		model = glm::mat4(1.0);
 		model = glm::translate(model, glm::vec3(-35.0f, 10.0f, 35.0f));
-		model = glm::scale(model, glm::vec3(4.0f, 4.0f, 4.0f));
+		modelaux = model;
 		model = glm::rotate(model, glm::radians(-45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(4.0f, 4.0f, 4.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		PuestoAlgodon.RenderModel();
 
+		//******************************** NPC Sam*************************************************************
+		model = modelaux;
+		model = glm::translate(model, glm::vec3(-15.0f, 1.3f, 0.0f));
+		model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(1.3f, 1.3f, 1.3f));
+		modelaux = model;
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Sam.RenderModel();
+
+		model = modelaux;
+		model = glm::translate(model, glm::vec3(0.4f, -1.5f, 0.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		PiernaIzq_Sam.RenderModel();
+
+		model = modelaux;
+		model = glm::translate(model, glm::vec3(-0.4f, -1.5f, 0.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		PiernaDer_Sam.RenderModel();
+
+		model = modelaux;
+		model = glm::translate(model, glm::vec3(1.2f, 2.72f, 0.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		BrazoIzq_Sam.RenderModel();
+
+		model = modelaux;
+		model = glm::translate(model, glm::vec3(-1.2f, 2.72f, 0.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		BrazoDer_Sam.RenderModel();
+
+		//*********************************************************************************************
 		model = glm::mat4(1.0);
 		model = glm::translate(model, glm::vec3(-35.0f, 12.0f, -35.0f));
 		modelaux = model;
